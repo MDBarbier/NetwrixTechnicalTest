82407a9d9cadff1a38ea637570ef471086fbb2f9
Matt Barbier <matthewdavidbarbier@gmail.com>
2020-02-10 20:39:04 +0000

    Refactoring

--- TechnicalTestApp/TechnicalTestApp/Controllers/ErrorsController.cs
+++ TechnicalTestApp/TechnicalTestApp/Controllers/ErrorsController.cs
@@ -1,28 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Diagnostics;
+﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TechnicalTestApp.Controllers
 {
     public class ErrorsController : Controller
     {
-        //[Route("Error/500")]
-        //public IActionResult Error500()
-        //{
-        //    var exceptionData = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-
-        //    if (exceptionData != null)
-        //    {
-        //        ViewBag.ErrorMessage = exceptionData.Error.Message;
-        //        ViewBag.RouteOfException = exceptionData.Path;
-        //    }
-
-        //    return View();
-        //}
-
         [Route("Error/{statusCode}")]
         public IActionResult HandleErrorCode(int statusCode)
         {
@@ -39,6 +21,8 @@ namespace TechnicalTestApp.Controllers
                     ViewBag.RouteOfException = statusCodeData.OriginalPath;
                     break;
                 default:
+                    ViewBag.ErrorMessage = "500 internal server error";
+                    ViewBag.RouteOfException = statusCodeData.OriginalPath;
                     break;
             }
 
